f07c592c9e4a950187587bf5e0352c126ec4847b
Ahmed Elhelow <ahmed.elhelow@gmail.com>
2020-02-14 15:57:38 +0200

    final

---
+++ init_unicycle_car_ineq.m
@@ -0,0 +1,20 @@
+%constant parameters of the kinematic models
+
+%initial state conditions
+
+x1 = 2;
+y1 = 2;
+th1 = pi/4;
+x2 = 0.9;
+y2 = 0.9;
+th2 = pi/4;
+x3 = 3.1;
+y3 = 0.9;
+th3 = 3*pi/4;
+
+dmin = 1;
+dmax = 2;
+
+%random seed fixed for the entire simulation
+seed = randi(10000);
+
